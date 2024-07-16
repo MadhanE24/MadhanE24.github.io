d2437bdec51a465e2404170f85d601cd7e1f8fa9
MadhanE24 <madhanezhilan@gmail.com>
2024-07-16 16:42:23 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -20,6 +20,8 @@ L.marker([35.2176665, -80.831473], {
 <hr>
 <a href="https://www.cpcc.edu">Click Here to Visit CPCC's Website</a><br>
 <iframe width="280" height="160" src="https://www.youtube.com/embed/nKrZk6cOcEU?si=8L1GS0j6gDhsodOG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<hr>
+<p>[URL: https://www.youtube.com/watch?v=nKrZk6cOcEU]</p>
 </center>
 `)
   .addTo(map);
